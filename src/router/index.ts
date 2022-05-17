680b2acbd41bc755678c7ca67cce1af23c1a84c1
BonTran <tt.bon.tran@tomosia.com>
2022-05-17 16:15:34 +0700

    Build interface Register page and task register for user

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import LoginUser from '../components/LoginUser.vue'
+import RegisterUser from '../components/RegisterUser.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'login',
     component: LoginUser,
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterUser,
   },
   {
     path: '/:pathMatch(.*)*',
